d674a40a8e511859c04a83090f08df5b4df03e55
Ronald Anthonisamy <ronchennai@live.com>
2020-03-05 23:29:10 -0500

    Added TextLogger

--- CSharpLessons/Program.cs
+++ CSharpLessons/Program.cs
@@ -8,8 +8,27 @@ namespace CSharpLessons
 {
     class Program
     {
+		
         static void Main(string[] args)
         {
+			TextLogger logger = new TextLogger();
+			try
+			{
+				throw new DivideByZeroException();
+			}
+			catch (Exception e)
+			{
+				logger.Log(e.Message);
+			}
         }
     }
+
+	class TextLogger
+	{
+		public void Log(string message)
+		{
+			Console.WriteLine("Log to a text file: " + message);
+		}
+	}
+	
 }
